6a0e6c4e4b44751f44ba3e8a81491cfd206145af
johnlenon213 <johnlenon@gmail.com>
2021-01-14 10:55:40 -0300

    tabelas

---
+++ database/migrations/2021_01_14_121453_create_vendas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendas', function (Blueprint $table) {
+            $table->id();
+            $table->string('datavendas');
+            $table->string('preçovendas');
+            $table->string('desconto');
+            $table->string('preço total');
+ 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendas');
+    }
+}
