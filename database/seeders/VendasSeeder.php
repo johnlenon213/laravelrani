ce56d473e7243435c70aed656ae0861813c2d3e4
johnlenon213 <johnlenon029@gmail.com>
2021-01-19 11:41:52 -0300

    sistema de autenticaçao

--- database/seeders/VendasSeeder.php
+++ database/seeders/VendasSeeder.php
@@ -30,13 +30,10 @@ class VendasSeeder extends Seeder
 
 
 
-it config --global user.email "mariac4682@gmail.com""
- 1756  git config --global user.email "mariac4682@gmail.com"
- 1757  git config --global user.name "Maria conceição"
- 1758  git config --global user.email "mariac4682@gmail,com"
- 1759  git log -3
- 1760  git add --all
- 1761  git --message "Criação do repositorio"
- 1762  git commit --message "Criação do repositorio"
- 1763  git add --all
- 1764  git push
+
+ //git config --global user.name "Maria conceição"
+ //git config --global user.email "mariac4682@gmail,com"
+ //git add --all
+ //git commit --message "Criação do repositorio"
+ //git add --all
+ //git push
